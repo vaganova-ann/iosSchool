c80b8acb318e540f123cce7f3bfe4aa56a583bd1
Anna Vaganova <vaganova.ann00@gmail.com>
2021-03-17 20:18:17 +0700

    Initial commit

---
+++ School/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  School
+//
+//  Created by Студент 2 on 17.03.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
