e400069c3279a084e946e3354edc77fb94cba821
Anna Vaganova <vaganova.ann00@gmail.com>
2021-03-19 21:07:11 +0700

    Lesson 2.

---
+++ School/CustomButton.swift
@@ -0,0 +1,27 @@
+//
+//  CustomButton.swift
+//  School
+//
+//  Created by Студент 2 on 19.03.2021.
+//
+
+import UIKit
+
+class CustomButton: UIButton{
+    override var isEnabled: Bool{
+//        set {
+//            self.isEnabled = newValue
+//        }
+//        get{
+//            return self.isEnabled
+//        }
+        didSet{
+            print(self.isEnabled)
+            foo(self.isEnabled)
+        }
+    }
+    
+    //var closure: () ->
+    func foo(_ isEnabled : Bool){}
+    
+}
--- School/ViewController.swift
+++ School/ViewController.swift
@@ -8,12 +8,45 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak private var titelLable: UILabel!
+    @IBOutlet weak private var loginButton: UIButton!
+    @IBOutlet weak private var loginTextField: UITextField!
+    @IBOutlet weak private var passwordTextField: UITextField!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        loginButton.addTarget(self, action: #selector(tapAction), for: .touchUpInside)
+        
+        loginButton.setTitle("код не войти", for: .disabled)
+        
+        let gestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(hideKeyboard))
+        view.addGestureRecognizer(gestureRecognizer)
+        
+        
+        //loginButton.isEnabled = false
+        
     }
-
-
+    
+    @objc func hideKeyboard(){
+        //view.resignFirstResponder() если строка ниже не помогает
+        view.endEditing(true)
+    }
+    
+    @objc func tapAction(){
+        guard let text = loginTextField.text else
+        {return}
+        
+        print("\(text) - текст с поля login")
+    }
+    
+    @IBAction func tapRegisterButttonAction() {
+        if let text = passwordTextField.text {
+            print("\(text) - текст с поля password")
+        }
+    }
+    
+    
 }
 
