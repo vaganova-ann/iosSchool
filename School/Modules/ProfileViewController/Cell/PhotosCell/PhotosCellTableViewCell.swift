752f0c379eaa0d01c32b5af74c4fd2b2dbbd7de6
Anna Vaganova <vaganova.ann00@gmail.com>
2021-04-08 12:38:13 +0700

    Has problems.

---
+++ School/Modules/ProfileViewController/Cell/PhotosCell/PhotosCellTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  PhotosCellTableViewCell.swift
+//  School
+//
+//  Created by Anna Vaganova on 4/6/21.
+//
+
+import UIKit
+
+class PhotosCellTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
---
+++ School/Modules/ProfileViewController/ProfileViewController.swift
@@ -0,0 +1,53 @@
+//
+//  ProfileViewController.swift
+//  School
+//
+//  Created by Anna Vaganova on 4/6/21.
+//
+
+import UIKit
+import KeychainSwift
+
+class ProfileViewController: UIViewController {
+    
+    @IBOutlet private var tableView: UITableView!
+    @IBOutlet private var logOutButton: UIButton!
+    
+    private var dataSource: ProfileTableViewDataSource!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let model = generateModel()
+        dataSource = ProfileTableViewDataSource(profile: model)
+        
+        tableView.dataSource = dataSource
+        tableView.delegate = dataSource
+        
+        let loginCellNib = UINib(nibName: LoginCell.className, bundle: Bundle.main)
+        tableView.register(loginCellNib, forCellReuseIdentifier: LoginCell.className)
+    }
+    
+    func generateModel() -> ProfileData {
+        
+        
+        if let autorizationToken = keyChain.get(ApplicationConstants.keychainTokenKey),
+           let responseData = AuthorizationMockSimulator().getProfile(token: autorizationToken)?.user
+        {
+            var userProfileData = ProfileData(login: responseData.login, registrationDate: responseData.registrationDate)
+            
+            if let responseColor = responseData.prefferedColor {
+                userProfileData.color = CustomColor(green: responseColor.green, red: responseColor.red, blue: responseColor.blue)
+            }
+            
+            if let responsePhoto = responseData.photo{
+                userProfileData.photo = responsePhoto
+            }
+            return userProfileData
+
+        }
+        return ProfileData(login: "Логин пользователя", registrationDate: Date())
+        }
+        
+}
+    
