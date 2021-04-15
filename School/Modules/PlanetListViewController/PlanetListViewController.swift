752628e78c349c023bfacdd7d85399c9233e2009
Anna Vaganova <vaganova.ann00@gmail.com>
2021-04-15 12:00:10 +0700

    Parallel query works.

--- School/Modules/PlanetListViewController/PlanetListViewController.swift
+++ School/Modules/PlanetListViewController/PlanetListViewController.swift
@@ -10,51 +10,50 @@ import PKHUD
 
 let networkService: PlanetsListNetworkService = NetworkService()
 
-class PlanetListViewController: UIViewController {
+class PlanetListViewController: UIViewController  {
     
     @IBOutlet private var tableView: UITableView!
+    
+    private var model: [DataAboutPlanet]!
+    
+    private var page: Int!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         HUD.registerForKeyboardNotifications()
-        //HUD.allowsInteraction = false
-        //HUD.dimsBackground = true
+        tableView.delegate = self
+        tableView.dataSource = self
         
+        model = []
+        page = 1
+        
+        loadPlanets(page: page, uiInteractionsAllowed: true)
+    
+        let planetCellNib = UINib(nibName: PlanetListCell.className, bundle: Bundle.main)
+        tableView.register(planetCellNib, forCellReuseIdentifier: PlanetListCell.className)
     }
     
-    func loadPlanets(page: Int) -> PlanetListResponceModel {
-        HUD.show(.progress)
+    func loadPlanets(page: Int, uiInteractionsAllowed: Bool) {
         
-        var resultResponse: PlanetListResponceModel!
-        
+        if uiInteractionsAllowed {
+            HUD.show(.progress)
+        }
         networkService.getPlanetList(page: page) { [weak self] (response, error) in
-            guard let self = self else { return }
-            HUD.hide()
-            //            self.textField.text = response?.info.next
-            print("--------RESPONSE---------------")
-            print(response as Any)
-            print("--------ERROR---------------")
-            print(error as Any)
-            print("--------END---------------")
+            guard let self = self,
+                  let resultResponse = response
+            else { return }
             
-            resultResponse = response
-        }
-        
-        return resultResponse // тут надо чота сделать посмотри в чате в тг
-    }
-    
-    override func viewDidAppear(_ animated: Bool) {
-        super.viewDidAppear(animated)
-        
-        DispatchQueue.main.async {
+            self.model.append(contentsOf: self.generateModel(planetList: resultResponse))
             
-        }
-        
-        DispatchQueue.global().async {
+            DispatchQueue.main.async {
+                self.tableView.reloadData()
+            }
             
+            if uiInteractionsAllowed {
+                HUD.hide()
+            }
         }
-        //loadPlanets()
     }
     
     deinit {
@@ -64,20 +63,41 @@ class PlanetListViewController: UIViewController {
     func generateModel(planetList : PlanetListResponceModel) -> [DataAboutPlanet]  {
         
         var planets: [DataAboutPlanet] = []
-        
         let informationAboutPlanet = planetList.results
-        
         for planet in informationAboutPlanet {
-            
             if let planetName = planet.name {
                 planets.append(DataAboutPlanet(name: planetName, type: planet.type, population: planet.residents.count))
             }
         }
         return planets
     }
-    
-    func loadingModel() {
-        // функция для подгрузки таблицы во время скролла
+}
+
+extension PlanetListViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
+        if indexPath.row == model.count/2 {
+            page += 1
+            DispatchQueue.global(qos: .userInitiated).async {
+                self.loadPlanets(page: self.page, uiInteractionsAllowed: false)
+            }
+        }
+    }
+}
+
+
+extension PlanetListViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        model.count
     }
     
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let planet = model[indexPath.row]
+        if let dequeuedCell = tableView.dequeueReusableCell(withIdentifier: PlanetListCell.className) as? PlanetListCell {
+            return dequeuedCell.createPlanetCell(information: planet)
+        }
+        else {
+            return UITableViewCell()
+        }
+    }
 }
